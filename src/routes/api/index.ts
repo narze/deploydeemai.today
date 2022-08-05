de8fa6228eef84765606e5b791ca020768048d9f
Manassarn Manoonchai <manassarn@gmail.com>
2022-08-05 23:57:33 +0700

    feat: add API

---
+++ src/routes/api/index.ts
@@ -0,0 +1,14 @@
+import { getReasons } from '$lib/helper';
+import type { RequestHandler } from './__types';
+
+export const GET: RequestHandler = async (event) => {
+	const day = event.url.searchParams.get('day');
+	const reasons = getReasons(day ? +day : undefined);
+	const reason = reasons[Math.floor(Math.random() * reasons.length)];
+
+	return {
+		body: {
+			message: reason
+		}
+	};
+};
