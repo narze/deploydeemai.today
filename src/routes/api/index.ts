8d522dd5eaa45c7cc19c8c8d261e7279b50a3002
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-10-04 06:52:46 +0700

    fix: deploydemmai is undefined

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -5,7 +5,7 @@ import type { RequestHandler } from './__types';
 
 export const GET: RequestHandler = async (event) => {
 	const day = event.url.searchParams.get('day');
-	const reasons = getReasons(day ? +day : undefined);
+	const { reasons, deploydeemai } = getReasons(day ? +day : undefined);
 	const reason = reasons[Math.floor(Math.random() * reasons.length)];
 
 	return {
