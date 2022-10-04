2701a7a73eeff6f65bbedadd9b9d368a2517b717
Sutthinart Khunvadhana <iakgoog@gmail.com>
2022-10-04 23:38:46 +0700

    Issue 3/api receive location (#6)
    Closes #3 
    * feat: #4 API should also return boolean value
    
    * fix: deploydemmai is undefined
    
    * feat: #3 API should be able to receive locale option

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,17 +1,21 @@
-import { getReasons } from '$lib/helper';
+import { getReasons, verifyTimezone } from '$lib/helper';
 import type { RequestHandler } from './__types';
 
 // TODO: receive timezone params
 
 export const GET: RequestHandler = async (event) => {
 	const day = event.url.searchParams.get('day');
-	const reasons = getReasons(day ? +day : undefined);
+	const { reasons, deploydeemai } = getReasons(day ? +day : undefined);
 	const reason = reasons[Math.floor(Math.random() * reasons.length)];
+
+	const tz = event.url.searchParams.get('tz') || '';
+	const timezone = verifyTimezone(tz) ? tz : 'Asia/Bangkok';
 
 	return {
 		body: {
 			message: reason,
-			deploydeemai
+			deploydeemai,
+			timezone
 		},
 	};
 };
