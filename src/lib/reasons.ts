612d8e7ffa7a9d07315fb3946f7b1e922e5bf627
Sirawit Sukwattanavit <56272903+sirawit-suk@users.noreply.github.com>
2022-10-24 14:19:08 +0700

    add we not deploy on friday (#30)

--- src/lib/reasons.ts
+++ src/lib/reasons.ts
@@ -105,6 +105,7 @@ export const FRIDAY_REASONS = [
 	'Deploy ศุกร์ ได้เปิดคอมถึงวันจันทร์เลยนะ',
 	'ร้านเหล้ารอท่านอยู้น้า',
 	'Deploy วันศุกร์บ้านบึ้ม',
+	'เราไม่ Deploy วันศุกร์นะ'
 ];
 
 export const REASONS_FOR_FRIDAY_13TH = [
